6d120cdbb2f811fbabc954b79542ffbcd2689eb0
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-02-08 17:43:11 +0530

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Button } from "@/components/ui/button"
+
+</script>
+
+<template>
+  <main>
+    <h1>Hello</h1>
+    <div class="p-4">
+      <Button>Click me</Button>
+    </div>
+  </main>
+</template>
